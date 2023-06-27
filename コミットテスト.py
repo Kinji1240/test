ae84cdce91a7c4385240af22cd2d6813b8af58cd
Kinji <sgwmr124@yahoo.co.jp>
2023-06-27 11:23:58 +0900

    0627_K

--- コミットテスト.py
+++ コミットテスト.py
@@ -1,29 +1,23 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.pagelayout import PageLayout
-from kivy.uix.button import Button
- 
-class MyApp(App):
-    title = 'My Application'
- 
-    def build(self):
-        ''' ビルド時に1回だけ実行される '''
- 
-        # レイアウトやウィジェットのインスタンス化
-        layout_root = PageLayout()
-        layout1 = BoxLayout(orientation='horizontal')
-        button1 = Button(text='Button1')
-        button2 = Button(text='Button2')
-        button3 = Button(text='Button3')
- 
-        # レイアウトにウィジェットを追加
-        layout1.add_widget(button2)
-        layout1.add_widget(button3)
-        layout_root.add_widget(button1)
-        layout_root.add_widget(layout1)
- 
-        return layout_root
- 
- 
-if __name__ == '__main__':
-    MyApp().run()
+import tkinter as tk
+import time
+
+# Tkinterのウィンドウを作成
+root = tk.Tk()
+root.title("Clock")
+
+# 時計のラベルを作成
+clock = tk.Label(root, font=("times", 50, "bold"))
+clock.pack()
+
+# 時計を更新する関数
+def tick():
+    # 現在の日時を取得
+    now = time.strftime("%H:%M:%S")
+    # ラベルのテキストを更新
+    clock.config(text=now)
+    # 1000msごとに再度tick関数を呼び出す
+    clock.after(1000, tick)
+
+# 時計をスタート
+tick()
+root.mainloop()
