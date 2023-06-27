bda06049d4718ef36e7b415d9f6f7561e8886b96
Kinji <sgwmr124@yahoo.co.jp>
2023-06-27 11:33:03 +0900

    0627_K

--- コミットテスト.py
+++ コミットテスト.py
@@ -1,23 +1,32 @@
-import tkinter as tk
-import time
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
-# Tkinterのウィンドウを作成
-root = tk.Tk()
-root.title("Clock")
 
-# 時計のラベルを作成
-clock = tk.Label(root, font=("times", 50, "bold"))
-clock.pack()
+class WidgetScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super(WidgetScreen, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        
+        # ラベル
+        label = Label(text='Hello, World!', font_size=24)
+        self.add_widget(label)
+        
+        # ボタン
+        button = Button(text='Click Me!', size_hint=(None, None), size=(150, 50))
+        button.bind(on_press=self.on_button_press)
+        self.add_widget(button)
+    
+    def on_button_press(self, instance):
+        print('Button Clicked!')
 
-# 時計を更新する関数
-def tick():
-    # 現在の日時を取得
-    now = time.strftime("%H:%M:%S")
-    # ラベルのテキストを更新
-    clock.config(text=now)
-    # 1000msごとに再度tick関数を呼び出す
-    clock.after(1000, tick)
 
-# 時計をスタート
-tick()
-root.mainloop()
+class WidgetApp(App):
+    def build(self):
+        return WidgetScreen()
+
+
+if __name__ == '__main__':
+    WidgetApp().run()
+    
