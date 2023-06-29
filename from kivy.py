44196a0bd355e109eba229f374628420de9ac32f
shuhei <204061@ocsjoho.onmicrosoft.com>
2023-06-29 14:24:33 +0900

    0629

--- from kivy.py
+++ from kivy.py
@@ -7,129 +7,32 @@ from kivy.uix.dropdown import DropDown
 
 
 class MyApp(App):
-    
-   
-def build(self):
-        layout = BoxLayout(orientation=
-       
-'vertical')
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
 
-        
-
-       
-
-
-# メニューボタン
-        menu_button = Button(text=
-        menu_button = Button(text
-
-        menu_button = Button
-
-        menu
-
-       
-'Menu', size_hint=(None, None))
+        # メニューボタン
+        menu_button = Button(text='Menu', size_hint=(None, None))
         menu_button.bind(on_release=self.show_menu)
 
-        
-        menu_button.bind(on_release=self.show_menu)
-
-       
-
-        menu_button.bind(on_release=self.show_menu)
-
-
-        menu_button.bind(on_release=self
-
-       
-# ドロップダウンメニュー
+        # ドロップダウンメニュー
         dropdown = DropDown()
-        menu_items = [
-        dropdown = DropDown()
-        menu_items =
-
-        dropdown = DropDown()
-        menu_items
-
-        dropdown = DropDown()
-        menu
-
-        dropdown = DropDown()
-       
-
-        dropdown = DropDown()
-
-       
-'Item 1', 'Item 2', 'Item 3']
-        
-       
-for item in menu_items:
-            btn = Button(text=item, size_hint_y=
-            btn = Button(text=item
-
-            btn = Button(text
-
-            btn =
-
-           
-None, height=40)
-            btn.bind(on_release=
-            btn.bind(on_release
-
-            btn
-
-           
-lambda btn: self.select_item(btn.text))
+        menu_items = ['Item 1', 'Item 2', 'Item 3']
+        for item in menu_items:
+            btn = Button(text=item, size_hint_y=None, height=40)
+            btn.bind(on_release=lambda btn: self.select_item(btn.text))
             dropdown.add_widget(btn)
 
         layout.add_widget(menu_button)
-        dropdown.bind(on_select=
-            dropdown.add_widget(btn)
-
-        layout.add_widget(menu_button)
-        dropdown.bind(on_select
-
-            dropdown.add_widget(btn)
-
-        layout.add_widget(menu_button)
-        dropdown
-
-            dropdown.add_widget(btn)
-
-        layout.add_widget(menu_button)
-       
-
-            dropdown.add_widget(btn)
-
-        layout.add_widget(menu_button)
-
-            dropdown.add_widget(btn)
-
-       
-
-            dropdown.add_widget(btn)
-
-
-           
-lambda instance, x: setattr(menu_button, 'text', x))
+        dropdown.bind(on_select=lambda instance, x: setattr(menu_button, 'text', x))
         return layout
 
-    
-
-def show_menu(self, button):
-        dropdown = button.parent.children[
-        dropdown = button
-0]
+    def show_menu(self, button):
+        dropdown = button.parent.children[0]
         dropdown.open(button)
 
     def select_item(self, text):
-        
-       
-print('Selected:', text)
+        print('Selected:', text)
 
 
 if __name__ == '__main__':
     MyApp().run()
-
-    MyApp().run()
-``
