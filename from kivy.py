b87bcea2c47f40563b896a2c91d2779723828550
shuhei <204061@ocsjoho.onmicrosoft.com>
2023-06-29 14:10:31 +0900

    0629

--- from kivy.py
+++ from kivy.py
@@ -1,34 +1,23 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
 from kivy.uix.spinner import Spinner
-from kivy.uix.button import Button
-
-
-class RootWidget(BoxLayout):
-    item_list = ObjectProperty(None)
-    label_text = ObjectProperty(None)
-    input_text = ObjectProperty(None)
-
-    def widgets_list_add(self):
-        self.item_list.values.append(self.input_text.text)
-
-    def label_text_change(self):
-        self.label_text.text = self.item_list.text
-
-
-class SpinnerButton(Button):
-    pass
-
 
 class MySpinner(Spinner):
-    option_cls = ObjectProperty(SpinnerButton)
+    def __init__(self, **kwargs):
+        super(MySpinner, self).__init__(**kwargs)
+        self.text = '都道府県を選択してください'
+        self.values = [
+            '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
+            '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
+            '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県',
+            '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県',
+            '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県',
+            '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県',
+            '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'
+        ]
 
-
-class TestSpinnerApp(App):
+class MyApp(App):
     def build(self):
-        return RootWidget()
-
+        return MySpinner()
 
 if __name__ == '__main__':
-    TestSpinnerApp().run()
+    MyApp().run()
