356e61b15b6fa54b4ba174311182874a504506e6
shuhei <204061@ocsjoho.onmicrosoft.com>
2023-06-29 14:22:20 +0900

    0629

--- from kivy.py
+++ from kivy.py
@@ -1,23 +1,135 @@
-from kivy.app import App
-from kivy.uix.spinner import Spinner
+# main.py
 
-class MySpinner(Spinner):
-    def __init__(self, **kwargs):
-        super(MySpinner, self).__init__(**kwargs)
-        self.text = '都道府県を選択してください'
-        self.values = [
-            '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県',
-            '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
-            '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県',
-            '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県',
-            '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県',
-            '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県',
-            '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'
-        ]
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
 
 class MyApp(App):
-    def build(self):
-        return MySpinner()
+    
+   
+def build(self):
+        layout = BoxLayout(orientation=
+       
+'vertical')
+
+        
+
+       
+
+
+# メニューボタン
+        menu_button = Button(text=
+        menu_button = Button(text
+
+        menu_button = Button
+
+        menu
+
+       
+'Menu', size_hint=(None, None))
+        menu_button.bind(on_release=self.show_menu)
+
+        
+        menu_button.bind(on_release=self.show_menu)
+
+       
+
+        menu_button.bind(on_release=self.show_menu)
+
+
+        menu_button.bind(on_release=self
+
+       
+# ドロップダウンメニュー
+        dropdown = DropDown()
+        menu_items = [
+        dropdown = DropDown()
+        menu_items =
+
+        dropdown = DropDown()
+        menu_items
+
+        dropdown = DropDown()
+        menu
+
+        dropdown = DropDown()
+       
+
+        dropdown = DropDown()
+
+       
+'Item 1', 'Item 2', 'Item 3']
+        
+       
+for item in menu_items:
+            btn = Button(text=item, size_hint_y=
+            btn = Button(text=item
+
+            btn = Button(text
+
+            btn =
+
+           
+None, height=40)
+            btn.bind(on_release=
+            btn.bind(on_release
+
+            btn
+
+           
+lambda btn: self.select_item(btn.text))
+            dropdown.add_widget(btn)
+
+        layout.add_widget(menu_button)
+        dropdown.bind(on_select=
+            dropdown.add_widget(btn)
+
+        layout.add_widget(menu_button)
+        dropdown.bind(on_select
+
+            dropdown.add_widget(btn)
+
+        layout.add_widget(menu_button)
+        dropdown
+
+            dropdown.add_widget(btn)
+
+        layout.add_widget(menu_button)
+       
+
+            dropdown.add_widget(btn)
+
+        layout.add_widget(menu_button)
+
+            dropdown.add_widget(btn)
+
+       
+
+            dropdown.add_widget(btn)
+
+
+           
+lambda instance, x: setattr(menu_button, 'text', x))
+        return layout
+
+    
+
+def show_menu(self, button):
+        dropdown = button.parent.children[
+        dropdown = button
+0]
+        dropdown.open(button)
+
+    def select_item(self, text):
+        
+       
+print('Selected:', text)
+
 
 if __name__ == '__main__':
     MyApp().run()
+
+    MyApp().run()
+``
