10ab5106974737bf2a6c2c10c4f32bb4644d0962
ishikawa <204004@ocsjoho.onmicrosoft.com>
2023-10-17 11:38:20 +0900

    2023/10/17

---
+++ testplay/font.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class MyKivyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        
+        # フォントファイルのパスを指定してフォントを使用
+        font_name = 'NotoSansCJKjp-Regular.otf'
+        
+        label = Label(text='Kivyサンプルアプリ', font_name=font_name, font_size=24)
+        
+        layout.add_widget(label)
+        return layout
+
+if __name__ == '__main__':
+    MyKivyApp().run()
