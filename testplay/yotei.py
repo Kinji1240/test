1eeb2c4c8ea80acb6e9f75d6f5a55419fcc1ef63
ishikawa <204004@ocsjoho.onmicrosoft.com>
2023-10-26 16:23:47 +0900

    2023/10/26/i

--- testplay/yotei.py
+++ testplay/yotei.py
@@ -1,35 +1,28 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivycalendar import DatePicker
+from kivy.uix.textinput import TextInput
 
-class EditableCalendarApp(App):
+class WeeklyCalendarApp(App):
     def build(self):
-        self.notes = {}  # 日付とメモの辞書
-
         layout = BoxLayout(orientation='vertical')
+        week_schedule = [
+            ("Sunday", ""),
+            ("Monday", ""),
+            ("Tuesday", ""),
+            ("Wednesday", ""),
+            ("Thursday", ""),
+            ("Friday", ""),
+            ("Saturday", "")
+        ]
 
-        # カレンダーウィジェットを作成
-        self.calendar = DatePicker()
-        self.calendar.bind(on_date=self.on_date_selected)
-        layout.add_widget(self.calendar)
-
-        # メモ表示用のラベル
-        self.memo_label = Label(text='メモ:')
-        layout.add_widget(self.memo_label)
-
-        # メモ入力用のテキストボックス
-        self.memo_input = Label()
-        layout.add_widget(self.memo_input)
-
+        for day, schedule in week_schedule:
+            day_label = Label(text=day, bold=True)
+            text_input = TextInput(text=schedule, multiline=False)
+            layout.add_widget(day_label)
+            layout.add_widget(text_input)
+        
         return layout
 
-    def on_date_selected(self, instance, value):
-        selected_date = value
-        memo = self.notes.get(selected_date, '')
-        self.memo_input.text = memo
-        self.memo_label.text = f'メモ ({selected_date}):'
-
 if __name__ == '__main__':
-    EditableCalendarApp().run()
+    WeeklyCalendarApp().run()
