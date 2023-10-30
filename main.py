85fc6a6a525b674f7a530d0d32cc2dc0a55bc95e
onoda <204012@ocsjoho.onmicrosoft.com>
2023-10-30 15:44:16 +0900

    2023/10/30

--- main.py
+++ main.py
@@ -1,48 +1,73 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-import os
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 import japanize_kivy
 
-
-class MainApp(App):
+class CalendarApp(App):
     def build(self):
-        layout = GridLayout(cols=1, spacing=10, padding=10)
+        self.layout = GridLayout(cols=7)
+        self.days = ["日", "月", "火", "水", "木", "金", "土"]
+        self.current_year = 2023 
+        self.current_month = 1
 
-        # タイトルのラベル
-        title_label = Label(
-            text="朝の目覚めにこのシステム",
-            font_size=24,
-            size_hint_y=None,
-            height=50,
-            halign="center",
-        )
+        self.create_calendar()
 
-        # サブタイトルのラベル
-        subtitle_label = Label(
-            text="Morning Pi",
-            font_size=16,
-            size_hint_y=None,
-            height=50,
-            halign="center",
-        )
+        control_layout = GridLayout(cols=3)
+        prev_month_button = Button(text="前月", on_release=self.prev_month)
+        next_month_button = Button(text="次月", on_release=self.next_month)
+        control_layout.add_widget(prev_month_button)
+        control_layout.add_widget(Label(text=f"{self.current_year}年{self.current_month}月"))
+        control_layout.add_widget(next_month_button)
 
-        button = Button(text="実行", size_hint=(None, None))
-        button.bind(on_press=self.launch_main2)
+        root_layout = GridLayout(rows=2)
+        root_layout.add_widget(control_layout)
+        root_layout.add_widget(self.layout)
 
-        layout.add_widget(Widget())  # 上部の余白用
-        layout.add_widget(title_label)
-        layout.add_widget(subtitle_label)
-        layout.add_widget(button)
+        return root_layout
 
-        return layout
+    def create_calendar(self):
+        self.layout.clear_widgets()
+        for day in self.days:
+            self.layout.add_widget(Label(text=day))
 
-    def launch_main2(self, instance):
-        # main2.pyを実行
-        os.system("python main2.py")
+        for day in range(1, 32):  # 1から31までの日付ボタンを作成
+            day_button = Button(text=str(day))
+            day_button.bind(on_release=self.show_popup)
+            self.layout.add_widget(day_button)
 
-if __name__ == "__main__":
-    MainApp().run()
+    def prev_month(self, instance):
+        if self.current_month == 1:
+            self.current_month = 12
+            self.current_year -= 1
+        else:
+            self.current_month -= 1
+        self.create_calendar()
+
+    def next_month(self, instance):
+        if self.current_month == 12:
+            self.current_month = 1
+            self.current_year += 1
+        else:
+            self.current_month += 1
+        self.create_calendar()
+
+    def show_popup(self, instance):
+        popup_content = GridLayout(rows=3)
+        popup_content.add_widget(TextInput(hint_text="予定を入力してください"))
+        popup_content.add_widget(Button(text="OK", on_release=self.close_popup))
+        popup_content.add_widget(Button(text="キャンセル", on_release=self.dismiss_popup))
+
+        self.popup = Popup(title="予定", content=popup_content, size_hint=(None, None), size=(300, 200))
+        self.popup.open()
+
+    def close_popup(self, instance):
+        self.popup.dismiss()
+
+    def dismiss_popup(self, instance):
+        self.popup.dismiss()
+
+if __name__ == '__main__':
+    CalendarApp().run()
--- onoD/onoD_test_scCh.py
+++ onoD/onoD_test_scCh.py
@@ -1,70 +1,56 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
 from kivy.uix.spinner import Spinner
-import japanize_kivy
+import calendar
 
 class CalendarApp(App):
     def build(self):
-        layout = GridLayout(cols=7, spacing=5, size_hint=(None, None), width=420)
-
+        layout = GridLayout(cols=7)
         self.days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
         for day in self.days:
-            layout.add_widget(Label(text=day, size_hint_y=None, height=44, color=(0.1, 0.5, 0.6, 1)))
+            layout.add_widget(Button(text=day, background_normal='', background_color=(0.1, 0.5, 0.6, 1)))
 
         self.day_buttons = []
 
-        for day_num in range(1, 32):
-            day_button = Button(text=str(day_num), size_hint=(None, None), size=(60, 60), background_color=(0.5, 0.7, 0.8, 1), on_release=self.show_popup)
+        for _ in range(35):
+            day_button = Button()
+            day_button.bind(on_release=self.show_popup)
             layout.add_widget(day_button)
             self.day_buttons.append(day_button)
 
-        self.spinner = Spinner(text='Select Month', values=[str(i) for i in range(1, 13)], size_hint=(None, None), size=(150, 44), pos_hint={'center_x': 0.5})
+        self.spinner = Spinner(text='Select Month', values=[calendar.month_name[i] for i in range(1, 13)])
         self.spinner.bind(text=self.on_spinner_select)
+        layout.add_widget(self.spinner)
 
-        self.layout = BoxLayout(orientation='vertical', spacing=5, padding=5)
-        self.layout.add_widget(self.spinner)
-        self.layout.add_widget(layout)
-
-        return self.layout
+        return layout
 
     def on_spinner_select(self, instance, value):
         selected_month = instance.text
-        days_in_month = int(selected_month)
+        days_in_month = calendar.monthrange(2023, list(calendar.month_name).index(selected_month))[1]
 
         for i, day_button in enumerate(self.day_buttons):
             if i < days_in_month:
+                day_button.text = str(i + 1)
                 day_button.disabled = False
             else:
+                day_button.text = ''
                 day_button.disabled = True
 
     def show_popup(self, instance):
-        if instance.text != '':
-            popup_content = BoxLayout(orientation='vertical', spacing=5, padding=5)
-            popup_content.add_widget(Label(text=f"予定 ({self.spinner.text}/{instance.text})", size_hint_y=None, height=44))
-            popup_content.add_widget(Button(text="予定を入力", on_release=self.show_input))
-            popup_content.add_widget(Button(text="閉じる", on_release=self.dismiss_popup))
+        popup_content = GridLayout(rows=3)
+        popup_content.add_widget(TextInput(hint_text="予定を入力してください"))
+        popup_content.add_widget(Button(text="OK", on_release=self.close_popup))
+        popup_content.add_widget(Button(text="キャンセル", on_release=self.dismiss_popup))
 
-            self.popup = Popup(title=f"予定 ({self.spinner.text}/{instance.text})", content=popup_content, size_hint=(None, None), size=(300, 150), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-            self.popup.open()
+        self.popup = Popup(title="予定", content=popup_content, size_hint=(None, None), size=(300, 200))
+        self.popup.open()
 
-    def show_input(self, instance):
+    def close_popup(self, instance):
         self.popup.dismiss()
-
-        input_popup_content = BoxLayout(orientation='vertical', spacing=5, padding=5)
-        input_popup_content.add_widget(TextInput(hint_text="予定を入力してください", size_hint_y=None, height=44))
-        input_popup_content.add_widget(Button(text="OK", on_release=self.dismiss_input_popup))
-
-        self.input_popup = Popup(title=f"予定入力", content=input_popup_content, size_hint=(None, None), size=(300, 150), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        self.input_popup.open()
-
-    def dismiss_input_popup(self, instance):
-        self.input_popup.dismiss()
 
     def dismiss_popup(self, instance):
         self.popup.dismiss()
