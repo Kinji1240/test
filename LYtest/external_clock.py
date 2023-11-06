18d80b9d2e85101cf4b8409f1c60f76a5b803b07
Kinji <sgwmr124@yahoo.co.jp>
2023-11-06 10:19:44 +0900

    1106

--- LYtest/external_clock.py
+++ LYtest/external_clock.py
@@ -1,15 +1,28 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from datetime import datetime
+from kivy.graphics import Color, Rectangle  # ColorとRectangleをインポート
 
-class ClockApp(BoxLayout):  # BoxLayoutのサブクラスとしてClockAppを定義
+class ClockApp(BoxLayout):
     def __init__(self, **kwargs):
         super(ClockApp, self).__init__(**kwargs)
-        self.orientation = 'horizontal'  # レイアウトを水平に配置
+        self.orientation = 'horizontal'
+        self.size_hint_x = 0.7
         self.label = Label(text='', font_size=50, halign='center')
         self.add_widget(self.label)
         Clock.schedule_interval(self.update_time, 1)
+        self.rect = None  # Rectangleオブジェクトを初期化
+
+    def on_size(self, instance, value):
+        # ウィジェットのサイズが変更されたら、背景キャンバスを設定
+        self.rect = Rectangle(pos=self.pos, size=self.size)
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(0, 0, 1, 1)  # 青色のRGBA値
+            if self.rect:
+                self.canvas.before.add(self.rect)
 
     def update_time(self, dt):
         now = datetime.now()
--- LYtest/main_app.py
+++ LYtest/main_app.py
@@ -4,6 +4,11 @@ from kivy.lang import Builder
 from external_clock import ClockApp  # 外部アプリのインポート
 from external_weather import WeatherApp  # 天気アプリのインポート
 
+from kivy.config import Config
+Config.set('graphics', 'width', '1920')  # ウィンドウの幅
+Config.set('graphics', 'height', '1080')  # ウィンドウの高さ
+
+
 kv = '''
 BoxLayout:
     orientation: 'horizontal'
