5294b81e78db47ddc8743987d8ebe1a236248bc5
shuhei <204061@ocsjoho.onmicrosoft.com>
2023-11-08 11:30:46 +0900

    2023/11/08

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-# -*- coding: utf-8 -*
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.uix.spinner import Spinner
 
-class CustomSpinner(Spinner):
-    pass
 
-class MainRoot(BoxLayout):
-
-    def on_spinner_change(self, text):
-        print('The spinner', self, 'have text', text)
-
-class MainApp(App):
+class MyApp(App):
     def build(self):
-        self.title = 'Spinner Test'
+        layout = GridLayout(cols=2, spacing=10)
 
-if __name__ == "__main__":
-    MainApp().run()
+        # ラベル
+        label = Label(text='item:', size_hint=(None, None))
+
+        # スピナー
+        spinner = Spinner(
+            text='item select',
+            values=('item1', 'item2', 'item3'),
+            size_hint=(None, None)
+        )
+        spinner.bind(text=self.on_spinner_select)
+
+        layout.add_widget(Label(text='item rei'))
+        layout.add_widget(Label())
+        layout.add_widget(label)
+        layout.add_widget(spinner)
+
+        return layout
+
+    def on_spinner_select(self, spinner, text):
+        print('item:', text)
+
+
+if __name__ == '__main__':
+    MyApp().run()
