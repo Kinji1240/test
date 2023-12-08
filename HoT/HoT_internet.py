1a81dbc5776737a2979d8e7af2a22eb716070d8c
shuhei <204061@ocsjoho.onmicrosoft.com>
2023-12-08 14:24:48 +0900

    2023/12/08

--- HoT/HoT_internet.py
+++ HoT/HoT_internet.py
@@ -1,74 +1,72 @@
-import csv
+import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+import subprocess
+import csv
+import os
 import japanize_kivy
-from plyer import wifi
 
-class InternetScreen(BoxLayout):
-    def __init__(self, **kwargs):
-        super(InternetScreen, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-
-        # インターネット接続ステータスを表示するラベル
-        self.status_label = Label(text='インターネットに接続しますか？', font_size='20sp', size_hint=(1, 0.5))
-        self.add_widget(self.status_label)
-
-        # はい・いいえボタンを配置するBoxLayout
-        button_layout = BoxLayout(size_hint=(1, 0.1))
-        self.add_widget(button_layout)
-
-        yes_button = Button(text='はい', size_hint=(0.5, 1), on_press=self.connect_to_internet)
-        button_layout.add_widget(yes_button)
-
-        no_button = Button(text='いいえ', size_hint=(0.5, 1), on_press=self.disconnect_from_internet)
-        button_layout.add_widget(no_button)
-
-        # CSVファイルのヘッダーを書き込む
-        with open('test/HoT/internet_data.csv', 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow(['操作', '結果'])
-
-    def connect_to_internet(self, instance):
-        print("インターネットに接続しました")
-        self.show_result_popup("インターネットに接続しました")
-        # CSVファイルにデータを書き込む
-        self.write_to_csv("はい", "成功")
-        # Wi-Fiの設定画面を開く（コメントアウトまたは削除）
-        # wifi.open_network_settings()
-
-    def disconnect_from_internet(self, instance):
-        print("インターネットに接続しませんでした")
-        self.show_result_popup("インターネットに接続しませんでした")
-        # CSVファイルにデータを書き込む
-        self.write_to_csv("いいえ", "失敗")
-
-    def write_to_csv(self, action, result):
-        # CSVファイルにデータを書き込む
-        with open('test/HoT/internet_data.csv', 'a', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow([action, result])
-
-    def show_result_popup(self, result):
-        popup_content = BoxLayout(orientation='vertical')
-        popup_content.add_widget(Label(text=result, font_size='18sp'))
-        close_button = Button(text='閉じる', size_hint=(1, 0.3), on_press=lambda instance: self.dismiss_popup(instance))
-        popup_content.add_widget(close_button)
-
-        # Popupインスタンスを保存
-        self._popup = Popup(title='結果', content=popup_content, size_hint=(0.6, 0.4))
-        self._popup.open()
-
-    def dismiss_popup(self, instance):
-        if isinstance(instance, Button):
-            # 保存したPopupインスタンスを閉じる
-            self._popup.dismiss()
-
-class InternetApp(App):
+class WiFiConnectApp(App):
     def build(self):
-        return InternetScreen()
+        self.csv_file_path = os.path.join("test", "HoT", "internet_data.csv")
 
-if __name__ == '__main__':
-    InternetApp().run()
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
+        wifi_list = self.get_wifi_list()
+        self.list_label = Label(text="Wi-Fi一覧:")
+        layout.add_widget(self.list_label)
+
+        for wifi in wifi_list:
+            button = Button(text=wifi, on_press=self.show_wifi_popup)
+            layout.add_widget(button)
+
+        return layout
+
+    def get_wifi_list(self):
+        # ここでWi-Fi一覧を取得する方法を実装する（例: subprocessモジュールを使用してiwlistコマンドを実行）
+        # この例では、仮のデータを返しています
+        return ["Wi-Fi1", "Wi-Fi2", "Wi-Fi3"]
+
+    def show_wifi_popup(self, instance):
+        selected_wifi = instance.text
+
+        # Popup内のコンテンツを構築
+        content_layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        connect_button = Button(text="接続する", on_press=lambda x: self.connect_wifi(selected_wifi))
+        disconnect_button = Button(text="接続しない", on_press=lambda x: self.disconnect_wifi())
+        content_layout.add_widget(connect_button)
+        content_layout.add_widget(disconnect_button)
+
+        # Popupを作成
+        popup = Popup(title=f"{selected_wifi}に接続", content=content_layout, size_hint=(None, None), size=(300, 200))
+        popup.open()
+
+    def connect_wifi(self, selected_wifi):
+        # ここで選択されたWi-Fiに接続する処理を実装する（例: subprocessモジュールを使用してwpa_supplicantを設定）
+
+        self.log_to_csv("接続成功", selected_wifi)
+        result_message = f"{selected_wifi} に接続しました。"
+        self.show_result_popup("接続成功", result_message)
+
+    def disconnect_wifi(self):
+        # ここでWi-Fi接続を切断する処理を実装する（例: subprocessモジュールを使用してwpa_cliを実行）
+
+        self.log_to_csv("切断成功", "N/A")
+        result_message = "Wi-Fi接続を切断しました。"
+        self.show_result_popup("切断成功", result_message)
+
+    def log_to_csv(self, action, wifi_name):
+        # CSVファイルにデータを追記
+        with open(self.csv_file_path, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([action, wifi_name])
+
+    def show_result_popup(self, title, message):
+        popup = Popup(title=title, content=Label(text=message), size_hint=(None, None), size=(400, 200))
+        popup.open()
+
+if __name__ == "__main__":
+    WiFiConnectApp().run()
